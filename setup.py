0cfe8b0afb6b71d211fe6f60858ba189e5effb83
Francesco Faraone <ffaraone@gaa9.com>
2015-08-26 13:18:41 +0200

    back port to python 2.7 up to 3.3

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
+import sys
 from setuptools import setup, find_packages
+
+
+PY34_PLUS = sys.version_info[0] == 3 and sys.version_info[1] >= 4
+
+exclude = ['tourbillon.uwsgi.uwsgi2'
+           if PY34_PLUS else 'tourbillon.uwsgi.uwsgi']
+
+install_requires = []
+
+if not PY34_PLUS:
+    install_requires.append('trollius==2.0')
+
+
 setup(
     name='tourbillon-uwsgi',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(exclude=exclude),
     zip_safe=False,
-    namespace_packages=['tourbillon']
+    namespace_packages=['tourbillon'],
+    install_requires=install_requires
 )
