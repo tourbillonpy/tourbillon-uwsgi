14b1b4d4189847d3d948484e5396ec2dda4f4061
Francesco Faraone <ffaraone@gmail.com>
2015-12-18 13:34:54 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -15,9 +15,27 @@ if not PY34_PLUS:
 
 setup(
     name='tourbillon-uwsgi',
+    description='A tourbillon plugin for collecting metrics from uWSGI.',
     version='0.4',
     packages=find_packages(exclude=exclude),
     zip_safe=False,
     namespace_packages=['tourbillon'],
-    install_requires=install_requires
+    install_requires=install_requires,
+    author='The Tourbillon Team',
+    author_email='tourbillonpy@gmail.com',
+    url='https://github.com/tourbillonpy/tourbillon-uwsgi',
+    license='ASF',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: System :: Monitoring',
+    ],
+    keywords='monitoring metrics agent influxdb uwsgi',
 )
